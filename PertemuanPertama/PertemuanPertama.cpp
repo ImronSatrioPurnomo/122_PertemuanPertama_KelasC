dab1817900c3cd4acb754773558cbcb1060cc4b1
ImronSatrioPurnomo <satriogames2223@gmail.com>
2024-02-26 11:26:08 +0700

    add variable jejari and luas

--- PertemuanPertama/PertemuanPertama.cpp
+++ PertemuanPertama/PertemuanPertama.cpp
@@ -3,8 +3,14 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello World!" << endl;
-	cout << "Hello" << endl;
+	//begin
+	    // numeric nJejari, nLuas
+	    // display ' Masukan Jejari= '
+	    // accept nJejari
+	    // compute nLuas = 3.14 * r * r
+	    // display 'Luasnya = ' +nLuas
+	//end
 
+	double nJejari, nLuas;
    system("pause");	
 }
